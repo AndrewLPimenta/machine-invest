d971718152506be535d8c81b28f3ba41afc79663
AndrewLPimenta <pimenta.andreews@gmail.com>
2025-02-15 15:42:50 -0300

    Teste

---
+++ README.md
@@ -0,0 +1,2 @@
+Front-End de um trabalho de faculdade, a proposta é um front-end de uma startup que atua no ramo
+# machine-invest
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Home from "./pages/Home";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Front-End Machine</h1>
+    </div>
+  );
+}
+
+export default App;
